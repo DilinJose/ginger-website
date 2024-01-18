f3858141d6cd066a407145425dfd24e3786bb833
DilinJose <dilinjose34@gmail.com>
2024-01-18 23:57:35 +0530

    responsive bug fixes design2

--- src/components/Designers/Designers.jsx
+++ src/components/Designers/Designers.jsx
@@ -20,34 +20,12 @@ const Designers = () => {
           <h6 className="designers-wrapper-two-desc">lorem Ipsam</h6>
         </div>
 
-        <div className="designers-wrapper-two-card border">
+        <div className="designers-wrapper-two-card">
           <img className="designers-wrapper-two-img" src={team1} />
           <h6 className="designers-wrapper-two-name">Sara Christian</h6>
           <h6 className="designers-wrapper-two-desc">lorem Ipsam</h6>
         </div>
         <div className="designers-wrapper-two-card">
-          {/* <div className="designers-wrapper-two-rectangle5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="14"
-              viewBox="0 0 32 14"
-              fill="none"
-            >
-              <path d="M32 0H0V14H32V0Z" fill="#FBB32E" />
-            </svg>
-          </div>
-          <div className="designers-wrapper-two-rectangle6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="35"
-              viewBox="0 0 14 35"
-              fill="none"
-            >
-              <path d="M14 35L14 0L0 0L0 35H14Z" fill="#FBB32E" />
-            </svg>
-          </div> */}
           <img className="designers-wrapper-two-img" src={team4} />
           <h6 className="designers-wrapper-two-name">Sara Christian</h6>
           <h6 className="designers-wrapper-two-desc">lorem Ipsam</h6>
